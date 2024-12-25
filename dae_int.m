8004062c17a164b574c89b46f8bd312f32675170
Lizandro Santos <108939593+LizandroCloud@users.noreply.github.com>
2024-12-25 17:24:12 -0300

    Delete dae_int.m

--- dae_int.m
+++
@@ -1,6 +0,0 @@
-function [t_s z] = dae_int(solver,function_handle,dt, z0, optODE )
-    % This function integrates da dae model according to a specific solver
-            [tspan zs] = ode45( function_handle,[dt], [z0], optODE );
-            t_s = tspan;
-            z = zs;
-end
